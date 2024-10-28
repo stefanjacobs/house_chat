55d1537078d5e8c77aa34550f1bd2df6f77609fa
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2024-10-28 21:56:47 +0100

    Fixed stupid dict bug

--- src/telegram_user_data.py
+++ src/telegram_user_data.py
@@ -9,21 +9,21 @@ async def reset_history(user_id):
     """
     Setze _nur_ die chat_history des Nutzers zurück
     """
+    global USER_DATA
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     current_time = datetime.datetime.now().strftime("%H:%M")
-    USER_DATA[user_id] = {
-        "chat_history": [
+    USER_DATA[user_id]["chat_history"] = [
             {"role": "system", "content": get_sysprompt(current_date, current_time)}
         ]
-    }
 
 
 async def create_user_data(user_id):
     """
     Erstellt ein Nutzerdatenobjekt
     """
-    USER_DATA[user_id] = {
-        "user_id": user_id,
-    }
+    global USER_DATA
+    if not user_id in USER_DATA:
+        USER_DATA[user_id] = dict()
+    USER_DATA[user_id]["user_id"] = user_id
     await reset_history(user_id)
 
